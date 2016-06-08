0356ba4ffc01a13cbe73440e58993905abd2e549
Nathan Toombs <nathan.toombs@classyllama.com>
2016-06-08 10:42:36 -0500

    #435_29 - Add dynamicEmbed functionality for Reviews and Q&A
    - Add all config fields for Review and Q&A
    - Update config field requirements
    - Add functions for retrieving new config field values
    - Update source model for setup type fields
    - Add config.phtml template to initialize TurnTo Social Commerce js
    - Add qa.phtml and reviews.phtml templates to display forms and data
    - Add teaser.phtml template to display teaser data and styles
    - Add layout to override core templates and include new templates
    - Add styles to style elements with core magento styles
    - Add Description plugin to remove Q&A tab from product details tabs if
    the Q&A functionality or SocialCommerce module is disabled
    - Add Review plugin to remove the count of reviews from the tab title

--- Helper/Config.php
+++ Helper/Config.php
@@ -2,50 +2,51 @@
 
 namespace TurnTo\SocialCommerce\Helper;
 
+use Magento\Store\Model\ScopeInterface;
+
 /**
  * Class Config - Assists with retrieval of TurnTo configuration settings
+ *
  * @package TurnTo\SocialCommerce\Helper
  */
 class Config extends \Magento\Framework\App\Helper\AbstractHelper
 {
     /**
-     * Configuration key for UPC to Product Attribute Mapping
+     * General Settings
      */
+    const XML_PATH_SOCIALCOMMERCE_ENABLED = 'turnto_socialcommerce_configuration/general/enabled';
+    
+    const XML_PATH_SOCIALCOMMERCE_SITE_KEY = 'turnto_socialcommerce_configuration/general/site_key';
+
+    const XML_PATH_SOCIALCOMMERCE_AUTHORIZATION_KEY = 'turnto_socialcommerce_configuration/general/authorization_key';
+    
+    const XML_PATH_SOCIALCOMMERCE_VERSION = 'turnto_socialcommerce_configuration/general/version';
+
+    const XML_PATH_SOCIALCOMMERCE_URL = 'turnto_socialcommerce_configuration/general/url';
+
+    const XML_PATH_SOCIALCOMMERCE_STATIC_URL = 'turnto_socialcommerce_configuration/general/static_url';
+
+    const XML_PATH_SOCIALCOMMERCE_AUTHENTICATION_KEY = 'turnto_socialcommerce_configuration/general/authentication_key';
+
+    /**
+     * Product Groups
+     */
+    const XML_PATH_SOCIALCOMMERCE_PRODUCT_GROUP = 'turnto_socialcommerce_configuration/product_attribute_mappings/';
+
     const UPC_ATTRIBUTE = 'upc_attribute';
 
-    /**
-     * Configuration key for MPN to Product Attribute Mapping
-     */
     const MPN_ATTRIBUTE = 'mpn_attribute';
 
-    /**
-     * Configuration key for ISBN to Product Attribute Mapping
-     */
     const ISBN_ATTRIBUTE = 'isbn_attribute';
 
-    /**
-     * Configuration key for EAN to Product Attribute Mapping
-     */
     const EAN_ATTRIBUTE = 'ean_attribute';
 
-    /**
-     * Configuration key for JAN to Product Attribute Mapping
-     */
     const JAN_ATTRIBUTE = 'jan_attribute';
 
-    /**
-     * Configuration key for ASIN to Product Attribute Mapping
-     */
     const ASIN_ATTRIBUTE = 'asin_attribute';
 
-    /**
-     * Configuration key for Brand to Product Attribute Mapping
-     */
     const BRAND_ATTRIBUTE = 'brand_attribute';
 
-    /**
-     * Array of the Product Attribute related mapping keys
-     */
     const PRODUCT_ATTRIBUTE_MAPPING_KEYS = [
         self::UPC_ATTRIBUTE,
         self::MPN_ATTRIBUTE,
@@ -57,41 +58,57 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
     ];
 
     /**
-     * XPATH to the TurnTo General Enabled Setting
+     * Questions and Answers
      */
-    const XML_PATH_ENABLED = 'turnto_socialcommerce_configuration/general/enabled';
+    const XML_PATH_SOCIALCOMMERCE_ENABLE_QA = 'turnto_socialcommerce_configuration/qa/enable_qa';
+
+    const XML_PATH_SOCIALCOMMERCE_ENABLE_QA_TEASER = 'turnto_socialcommerce_configuration/qa/enable_qa_teaser';
+
+    const XML_PATH_SOCIALCOMMERCE_SETUP_TYPE = 'turnto_socialcommerce_configuration/question_answer/setup_type';
+    
+    /**
+     * Reviews
+     */
+    const XML_PATH_SOCIALCOMMERCE_ENABLE_REVIEWS = 'turnto_socialcommerce_configuration/reviews/enable_reviews';
+
+    const XML_PATH_SOCIALCOMMERCE_ENABLE_REVIEWS_TEASER = 'turnto_socialcommerce_configuration/reviews/enable_reviews_teaser';
+
+    const XML_PATH_SOCIALCOMMERCE_REVIEWS_SETUP_TYPE = 'turnto_socialcommerce_configuration/reviews/reviews_setup_type';
 
     /**
-     * XPATH to the TurnTo General Site Key Setting
+     * Product Feed
      */
-    const XML_PATH_SITE_KEY = 'turnto_socialcommerce_configuration/general/site_key';
+    const XML_PATH_SOCIALCOMMERCE_ENABLE_PRODUCT_FEED_SUBMISSION = 'turnto_socialcommerce_configuration/product_feed/enable_automatic_submission';
+    
+    const XML_PATH_SOCIALCOMMERCE_FEED_SUBMISSION_URL = 'turnto_socialcommerce_configuration/product_feed/feed_submission_url';
 
 
     /**
-     * XPATH to the TurnTo General API Version Setting
+     * @var \Magento\Store\Model\StoreManagerInterface|null
      */
-    const XML_PATH_VERSION = 'turnto_socialcommerce_configuration/general/version';
-
-
-    /**
-     * XPATH to the TurnTo General Authorization Key Setting
-     */
-    const XML_PATH_AUTHORIZATION_KEY = 'turnto_socialcommerce_configuration/general/authorization_key';
+    protected $storeManager = null;
 
     /**
-     * XPATH to the TurnTo Product Feed Automatic Submission Enabled Setting
+     * Config constructor.
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      */
-    const XML_PATH_ENABLE_PRODUCT_FEED_SUBMISSION = 'turnto_socialcommerce_configuration/product_feed/enable_automatic_submission';
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    ) {
+        $this->storeManager = $storeManager;
+        parent::__construct($context);
+    }
 
     /**
-     * XPATH to the TurnTo Product Feed Submission URL Setting
+     * Gets the store code from the currently set/scoped store
+     * @return string
      */
-    const XML_PATH_FEED_SUBMISSION_URL = 'turnto_socialcommerce_configuration/product_feed/feed_submission_url';
-
-    /**
-     * XPATH to the TurnTo Product Attribute Mappings Setting Group
-     */
-    const XML_PATH_PRODUCT_GROUP = 'turnto_socialcommerce_configuration/product_attribute_mappings/';
+    protected function getCurrentStoreCode()
+    {
+        return $this->storeManager->getStore()->getCode();
+    }
 
     /**
      * Gets the value of the setting that determines if TurnTo's configuration is enabled
@@ -100,73 +117,129 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
      * @param $scopeCode
      * @return mixed
      */
-    public function getIsEnabled ($scopeType, $scopeCode)
+    public function getIsEnabled ($store = null)
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_ENABLED, $scopeType, $scopeCode);
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_ENABLED,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
     }
 
     /**
      * Gets the value of the setting that determines if automated Product Feed Submission is enabled
      *
-     * @param $scopeType
-     * @param $scopeCode
+     * @param $store = null
      * @return mixed
      */
-    public function getIsProductFeedSubmissionEnabled ($scopeType, $scopeCode)
+    public function getIsProductFeedSubmissionEnabled ($store = null)
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_ENABLE_PRODUCT_FEED_SUBMISSION, $scopeType, $scopeCode);
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_ENABLE_PRODUCT_FEED_SUBMISSION,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
     }
 
     /**
      * Gets the TurnTo Site Key
      *
-     * @param $scopeType
-     * @param $scopeCode
      * @return mixed
      */
-    public function getSiteKey ($scopeType, $scopeCode)
+    public function getSiteKey ($store = null)
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_SITE_KEY, $scopeType, $scopeCode);
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_SITE_KEY,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
     }
 
     /**
      * Gets the TurnTo API Version
      *
-     * @param $scopeType
-     * @param $scopeCode
+     * @param $store = null
      * @return mixed
      */
-    public function getTurnToVersion ($scopeType, $scopeCode)
+    public function getTurnToVersion ($store = null)
     {
         return str_replace(
             '.',
             '_',
-            $this->scopeConfig->getValue(self::XML_PATH_VERSION, $scopeType, $scopeCode)
+            $this->scopeConfig->getValue(
+                self::XML_PATH_SOCIALCOMMERCE_VERSION,
+                ScopeInterface::SCOPE_STORE,
+                isset($store) ? $store : $this->getCurrentStoreCode()
+            )
         );
+    }
+    
+    public function getStaticUrl($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_STATIC_URL,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+    public function getUrl($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_URL,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+    public function getStaticUrlWithoutProtocol($store = null)
+    {
+        return $this->removeProtocol($this->getStaticUrl($store));
+    }
+
+    public function getUrlWithoutProtocol($store = null)
+    {
+        return $this->removeProtocol($this->getUrl($store));
+    }
+
+    function removeProtocol($url)
+    {
+        $disallowed = array('http://', 'https://');
+        foreach($disallowed as $d) {
+            if(strpos($url, $d) === 0) {
+                return str_replace($d, '', $url);
+            }
+        }
+        return $url;
     }
 
     /**
      * Gets the TurnTo API Authorization Key
      *
-     * @param $scopeType
-     * @param $scopeCode
      * @return mixed
      */
-    public function getAuthorizationKey ($scopeType, $scopeCode)
+    public function getAuthorizationKey ($store = null)
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_AUTHORIZATION_KEY, $scopeType, $scopeCode);
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_AUTHENTICATION_KEY,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
     }
 
     /**
      * Gets the TurnTo URL to send a feed to
      *
-     * @param $scopeType
-     * @param $scopeCode
      * @return mixed
      */
-    public function getFeedUploadAddress ($scopeType, $scopeCode)
+    public function getFeedUploadAddress ($store = null)
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_FEED_SUBMISSION_URL, $scopeType, $scopeCode);
+        //return 'https://www.turnto.com/feedUpload/postfile';
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_FEED_SUBMISSION_URL,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
     }
 
     /**
@@ -177,28 +250,100 @@ class Config extends \Magento\Framework\App\Helper\AbstractHelper
      * @param $scopeCode
      * @return mixed
      */
-    public function getProductAttributeMapping ($mappingKey, $scopeType, $scopeCode)
+    public function getProductAttributeMapping ($mappingKey, $store)
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_PRODUCT_GROUP . $mappingKey, $scopeType, $scopeCode);
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_PRODUCT_GROUP . $mappingKey,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
     }
 
     /**
      * Gets an associative array of any set product attributes related to GTIN, key => mappingKey, value => attr_code
      *
-     * @param $scopeType
-     * @param $scopeCode
      * @return array
      */
-    public function getGtinAttributesMap ($scopeType, $scopeCode)
+    public function getGtinAttributesMap ($store = null)
     {
         $gtinMap = [];
         foreach (self::PRODUCT_ATTRIBUTE_MAPPING_KEYS as $mappingKey) {
             $tempResult = null;
-            $tempResult = $this->getProductAttributeMapping($mappingKey, $scopeType, $scopeCode);
+            $tempResult = $this->getProductAttributeMapping(
+                $mappingKey,
+                ScopeInterface::SCOPE_STORE,
+                isset($store) ? $store : $this->getCurrentStoreCode()
+            );
             if (!empty($tempResult)) {
                 $gtinMap[$mappingKey] = $tempResult;
             }
         }
         return $gtinMap;
     }
+
+    public function getQaEnabled($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_ENABLE_QA,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+    public function getQaTeaserEnabled($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_ENABLE_QA_TEASER,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+    /**
+     * Gets the Question and Answer Setup Type configuration value
+     *
+     * @return mixed
+     */
+    public function getSetupType($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_SETUP_TYPE,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+    public function getReviewsEnabled($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_ENABLE_REVIEWS,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+    public function getReviewsTeaserEnabled($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_ENABLE_REVIEWS_TEASER,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+    /**
+     * Gets the Reviews Setup Type configuration value
+     *
+     * @return mixed
+     */
+    public function getReviewsSetupType($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_SOCIALCOMMERCE_REVIEWS_SETUP_TYPE,
+            ScopeInterface::SCOPE_STORE,
+            isset($store) ? $store : $this->getCurrentStoreCode()
+        );
+    }
+
+
 }
---
+++ Plugin/Catalog/Block/Product/View/Description.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TurnTo\SocialCommerce\Plugin\Catalog\Block\Product\View;
+
+use Magento\Catalog\Block\Product\View\Description as OriginalDescription;
+use TurnTo\SocialCommerce\Helper\Config;
+
+class Description
+{
+    protected $config;
+
+    public function __construct(
+        Config $config
+    ) {
+        $this->config = $config;
+    }
+
+    /**
+     * Removes the Questions tab from the product details section
+     *
+     * This plugin will remove the Q&A tab from the product details tabs section if the Enable Q&A config field is set
+     * to no or if Enable Social Commerce is set to no. It is done this way rather than on the block definition because
+     * you can only have one ifconfig attribute. This was done in a plugin rather than in the template that renders all
+     * blocks assigned to the detailed_info group to prevent conflicts with other modules or themes.
+     *
+     * @param OriginalQuestions $subject
+     * @param $result
+     */
+    public function afterGetGroupChildNames(OriginalDescription $subject, $result)
+    {
+        if (!$this->config->getQaEnabled() || !$this->config->getIsEnabled()) {
+            $result = array_diff($result, array('qa.tab'));
+        }
+        return $result;
+    }
+}
+?>
+
