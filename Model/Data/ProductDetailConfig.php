26aaa9d1d0542236836fbca8f23571349c35fd80
Sean Templeton <sean@templeton.io>
2018-04-10 16:37:55 -0500

    Refactor config generation and migrate pinboard to new generation method

---
+++ Model/Data/ProductDetailConfig.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category    ClassyLlama
+ * @package
+ * @copyright   Copyright (c) 2018 Classy Llama Studios, LLC
+ */
+
+namespace TurnTo\SocialCommerce\Model\Data;
+
+class ProductDetailConfig
+{
+
+}
