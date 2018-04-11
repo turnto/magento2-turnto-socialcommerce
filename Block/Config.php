e40c7863b70577713c477710bff707e453672012
Sean Templeton <sean@templeton.io>
2018-04-11 13:09:12 -0500

    Remove unused method in config block

--- Block/Config.php
+++ Block/Config.php
@@ -15,7 +15,7 @@
 
 namespace TurnTo\SocialCommerce\Block;
 
-use \Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 
 class Config extends \Magento\Catalog\Block\Product\View\Description
 {
@@ -53,74 +53,6 @@ class Config extends \Magento\Catalog\Block\Product\View\Description
         $this->localeResolver = $localeResolver;
         parent::__construct($context, $registry, $data);
         $this->_product = $this->getProduct();
-    }
-
-    /**
-     * Build config json to be assigned to the turnToConfig variable
-     *
-     * @param bool $skipCssLoad
-     * @param bool $loadQA
-     * @param bool $loadReviews
-     * @param bool $loadCC
-     * @param bool $loadSSO
-     * @return string
-     */
-    public function getConfigJson(
-        $skipCssLoad = true,
-        $loadQA = true,
-        $loadReviews = true,
-        $loadCC = true,
-        $loadSSO = true
-    ){
-        $config = [
-            'siteKey' => $this->config->getSiteKey(),
-            'host' => $this->config->getUrlWithoutProtocol(),
-            'staticHost' => $this->config->getStaticUrlWithoutProtocol(),
-            'skipCssLoad' => $skipCssLoad,
-        ];
-
-        if ($this->config->getQaEnabled() && $loadQA) {
-            $config['setupType'] = $this->config->getSetupType();
-            if ($this->config->getQaTeaserEnabled()) {
-                $config['iTeaserFunc'] = new \Zend_Json_Expr('qaTeaser');
-            }
-        }
-
-        if ($this->config->getReviewsEnabled() && $loadReviews) {
-            $config['reviewsSetupType'] = $this->config->getReviewsSetupType();
-            if ($this->config->getReviewsTeaserEnabled()) {
-                $config['reviewsTeaserFunc'] = new \Zend_Json_Expr('reviewsTeaser');
-            }
-        }
-
-        if ($this->config->getCheckoutCommentsEnabledProductDetail() && $loadCC) {
-            $config['chatter'] = [
-                'minimumCommentCount' => 1,
-                'minimumCommentCharacterCount' => 1,
-                'minimumCommentWordCount' => 1,
-                'columns' => $this->config->getColumns()
-            ];
-        }
-
-        if ($this->config->getSingleSignOn() && $loadSSO) {
-            $config['registration'] = [
-                'localGetLoginStatusFunction' => new \Zend_Json_Expr('localGetLoginStatusFunction'),
-                'localRegistrationUrl' => $this->getBaseUrl() . 'turnto/sso/login',
-                'localGetUserInfoFunction' => new \Zend_Json_Expr('localGetUserInfoFunction'),
-                'localLogoutFunction' => new \Zend_Json_Expr('localLogoutFunction')
-            ];
-        }
-
-        if (is_array($this->config->getCustomConfigurationJs())) {
-            $config = array_merge($config, $this->config->getCustomConfigurationJs());
-        }
-
-        /*
-         * Zend_Json::encode is used instead of json_encode because the values of iTeaserFunc and reviewsTeaserFunc
-         * have to be a JavaScript object. json_encode has no way to accomplish this. See this stack overflow question
-         * for more context http://stackoverflow.com/questions/6169640/php-json-encode-encode-a-function
-         */
-        return \Zend_Json::encode($config, false, ['enableJsonExprFinder' => true]);
     }
 
     /**
