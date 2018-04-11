c87b038f69b86c68b768d68448942d9d718c2578
Sean Templeton <sean@templeton.io>
2018-04-11 13:09:49 -0500

    Code formatting

--- Block/Config.php
+++ Block/Config.php
@@ -1,14 +1,11 @@
 <?php
 /**
  * TurnTo_SocialCommerce
- *
  * NOTICE OF LICENSE
- *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
- *
  * @copyright  Copyright (c) 2018 TurnTo Networks, Inc.
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
@@ -35,12 +32,11 @@ class Config extends \Magento\Catalog\Block\Product\View\Description
     protected $localeResolver;
 
     /**
-     * Config constructor.
      * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \TurnTo\SocialCommerce\Helper\Config $config
-     * @param \Magento\Framework\Locale\Resolver $localeResolver
-     * @param array $data
+     * @param \Magento\Framework\Registry                      $registry
+     * @param \TurnTo\SocialCommerce\Helper\Config             $config
+     * @param \Magento\Framework\Locale\Resolver               $localeResolver
+     * @param array                                            $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
@@ -48,10 +44,12 @@ class Config extends \Magento\Catalog\Block\Product\View\Description
         \TurnTo\SocialCommerce\Helper\Config $config,
         \Magento\Framework\Locale\Resolver $localeResolver,
         array $data
-    ) {
+    )
+    {
+        parent::__construct($context, $registry, $data);
+
         $this->config = $config;
         $this->localeResolver = $localeResolver;
-        parent::__construct($context, $registry, $data);
         $this->_product = $this->getProduct();
     }
 
