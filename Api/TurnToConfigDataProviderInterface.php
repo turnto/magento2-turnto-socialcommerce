4ffa2fb191d115ef0f997b515344eb850f811dfa
Sean Templeton <sean@templeton.io>
2018-04-12 09:35:52 -0500

    Fix backwards compatibility

--- Api/TurnToConfigDataProviderInterface.php
+++ Api/TurnToConfigDataProviderInterface.php
@@ -5,15 +5,25 @@
  * @copyright   Copyright (c) 2018 Classy Llama Studios, LLC
  */
 
-namespace TurnTo\SocialCommerce\Api;
+// In Magento 2.2.x, Magento introduced a required interface for block arguments, add this for backwards compatibility
+namespace Magento\Framework\View\Element\Block {
 
-interface TurnToConfigDataProviderInterface
-{
-    /**
-     * Returns TurnTo configuration data used in the JavaScript snippet
-     *
-     * @api
-     * @return array
-     */
-    public function getData();
+    if (!interface_exists('\Magento\Framework\View\Element\Block\ArgumentInterface')) {
+        interface ArgumentInterface
+        {
+        }
+    }
+}
+
+namespace TurnTo\SocialCommerce\Api {
+
+    interface TurnToConfigDataProviderInterface
+    {
+        /**
+         * Returns TurnTo configuration data used in the JavaScript snippet
+         * @api
+         * @return array
+         */
+        public function getData();
+    }
 }
