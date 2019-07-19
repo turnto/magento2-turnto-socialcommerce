12a83001ebb08461d8baf1fd24a03e125cc9c426
Josh Fedoryszyn <josh.fedoryszyn@classyllama.com>
2019-07-19 13:10:53 -0500

    TTO-495_65 - Redirect to login with message

--- Controller/SSO/RedirectToLogin.php
+++ Controller/SSO/RedirectToLogin.php
@@ -8,7 +8,56 @@
 namespace TurnTo\SocialCommerce\Controller\SSO;
 
 
-class RedirectToLogin
+use Magento\Framework\App\Action\Context;
+
+class RedirectToLogin extends  \Magento\Framework\App\Action\Action
 {
 
+    const REVIEW_MSG = "We need to know who you are before we post your review. Please login or register to continue.";
+    const QUESTION_MSG = "Your question has been submitted. Please login or register to have answers emailed to you.";
+    const ANSWER_MSG =  "Please login or register to complete your submission.";
+    const REPLY_MSG =  "Please login or register to complete your submission.";
+
+    /**
+     * @var Magento\Framework\UrlInterface
+     */
+    private $uriInterface;
+
+    public function __construct(Context $context, \Magento\Framework\Message\ManagerInterface $messageManager, \Magento\Framework\UrlInterface $uriInterface) {
+        $this->messageManager = $messageManager;
+        parent::__construct($context);
+        $this->uriInterface = $uriInterface;
+    }
+
+    public function execute() {
+        $url = $this->_redirect->getRefererUrl();
+
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $login_url = $this->uriInterface
+            ->getUrl('customer/account/login',
+                array('referer' => base64_encode($url))
+            );
+        $resultRedirect->setPath($login_url);
+        $this->messageManager->addErrorMessage($this->getMessage());
+        return $resultRedirect;
+    }
+
+    public function getMessage(){
+       $action =  $this->getRequest()->getParam('action');
+       switch ($action){
+           case "QUESTION_CREATE":
+               return self::QUESTION_MSG;
+           case "ANSWER_CREATE":
+               return self::ANSWER_MSG;
+           case "REVIEW_CREATE":
+               return self::REVIEW_MSG;
+           case "REPLY_CREATE":
+               return self::REPLY_MSG;
+           default:
+               return "";
+       }
+
+    }
+
 }
+
