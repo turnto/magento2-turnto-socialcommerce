dd48a4b90dbff6b5ecd43f558635f026fad6c714
Josh Fedoryszyn <josh.fedoryszyn@classyllama.com>
2019-07-19 12:42:50 -0500

    TTO-495_65- redirect with message to login

---
+++ Controller/SSO/RedirectToLogin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @category    ClassyLlama
+ * @package
+ * @copyright   Copyright (c) 2019 Classy Llama Studios, LLC
+ */
+
+namespace TurnTo\SocialCommerce\Controller\SSO;
+
+
+class RedirectToLogin
+{
+
+}
