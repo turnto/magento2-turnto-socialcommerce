9a3be754230c72d61c9db8b96d4bfeae2baaedee
Josh Fedo <joshfedoCL@bitbucket.org>
2020-05-15 14:12:38 -0500

    TTOR01-5 - implemented changes

---
+++ Controller/SSO/LoggedInData.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * @category    ClassyLlama
+ * @package
+ * @copyright   Copyright (c) 2020 Classy Llama Studios, LLC
+ */
+
+namespace TurnTo\SocialCommerce\Controller\SSO;
+
+
+use Magento\Framework\Controller\ResultFactory;
+use TurnTo\SocialCommerce\Helper\firebase\JWT;
+
+class LoggedInData
+{
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $customerSession;
+
+    /**
+     * @var \Magento\Framework\Controller\ResultFactory
+     */
+    protected $resultFactory;
+
+    /**
+     * @var \TurnTo\SocialCommerce\Helper\Config
+     */
+    protected $configHelper;
+
+    /**
+     * GetUserStatus constructor.
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \TurnTo\SocialCommerce\Helper\Config $configHelper
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        \TurnTo\SocialCommerce\Helper\Config $configHelper
+    ) {
+        $this->customerSession = $customerSession;
+        $this->resultFactory = $context->getResultFactory();
+        $this->configHelper = $configHelper;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $customer = $this->customerSession->getCustomer();
+
+        if ($customer->getId()) {
+            $customerData = [
+                'payload' => [
+                    //add unique key
+                    'ua' => $customer->getId(),
+                    'iss' => 'TurnTo',
+                    'exp' => time() + 86400
+                ]
+            ];
+        } else {
+            $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+            $resultJson->setData(['jwt'=>null]);
+            return $resultJson;
+
+        }
+
+        if (!is_null($customerData['payload']['user_auth_token'])) {
+            $customerData['signature'] = $this->getSignature($customerData['payload']);
+        }
+
+
+        $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $resultJson->setData(['jwt' => $this->getUserJWTToken($customerData['payload'])]);
+
+        return $resultJson;
+    }
+
+    /**
+     * Creates a signature that authenticates the user information with TurnTo's servers
+     * https://docs.google.com/document/d/1AEpganoUMVbDyqlYBctN9m9r3J8v8hug5haFkX81934/edit
+     * Section: Computing the signature
+     *
+     * @param $customerData
+     * @return mixed
+     */
+    public function getSignature($customerData) {
+        // Sort the fields on user alphabetically
+        ksort($customerData);
+        $params = urldecode(http_build_query($customerData, null, '&', PHP_QUERY_RFC3986));
+        $authKey = $this->configHelper->getAuthorizationKey();
+
+        // Hash the parameter string and base64 encode it because the hash result is binary
+        $signature = base64_encode(hash_hmac('sha256', $params, $authKey, true));
+
+        return $signature;
+    }
+
+    public function getUserJWTToken($customer){
+
+        if(!$customer) {
+            return "error";
+        }
+
+        return JWT::encode($customer, $this->configHelper->getAuthorizationKey(), 'HS256');
+    }
+}
---
+++ view/frontend/web/js/turnto-config.js
@@ -0,0 +1,63 @@
+/**
+ * TurnTo_SocialCommerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright  Copyright (c) 2018 TurnTo Networks, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+define([
+    'jquery',
+], function ($) {
+    'use strict';
+    return function (config) {
+
+
+        if (window.turnToConfig.hasOwnProperty('sso')
+        ) {
+            window.turnToConfig.sso.userDataFn = function (contextObj) {
+                    $.get(window.turnToConfig.baseUrl + 'turnto/sso/getuserstatus', function (data) {
+                    if (data.jwt === null) {
+                        let context = JSON.parse(atob(contextObj));
+                        window.sessionStorage.setItem('contextObj', contextObj)
+                        window.location.replace("/turnto/sso/redirecttologin/action/" + context.action + '/authSetting/' + context.authSetting);
+                    } else if (window.sessionStorage.getItem('contextObj')) {
+                        window.TurnToCmd('ssoRegDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: data.jwt});
+                        window.sessionStorage.removeItem('contextObj')
+                    } else {
+                        //this may not be needed . Test when sso comes back online
+                        window.TurnToCmd('ssoRegDone', {context: contextObj, userDataToken: data.jwt});
+                    }
+                })
+
+            };
+
+            //why is this duplicated? possible to run outside of userdatafn call?
+            if(window.sessionStorage.getItem('contextObj')){
+                $.get(window.turnToConfig.baseUrl + 'turnto/sso/getuserstatus', function (data) {
+                    window.TurnToCmd('ssoRegDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: data.jwt});
+                    window.sessionStorage.removeItem('contextObj');
+                })
+            }
+
+            window.turnToConfig.sso.loggedInDataFn = function(contextObj) {
+                $.get(window.turnToConfig.baseUrl + 'turnto/sso/LoggedInData', function (data) {
+                        window.TurnToCmd('loggedInDataFnDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: data.jwt});
+                        window.TurnToCmd('loggedInDataFnDone', {context: contextObj, userDataToken: data.jwt});
+                    }
+                )
+            };
+            window.turnToConfig.sso.logout =  function() {
+                window.location.replace("/customer/account/logout");
+            };
+
+        }
+
+
+    }
+});
