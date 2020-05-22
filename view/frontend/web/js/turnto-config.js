8a12a493ed61da4b58ff9006367482055b521b68
Josh Fedo <joshfedoCL@bitbucket.org>
2020-05-22 09:14:52 -0500

    TTOR01-5 - added error handling

--- view/frontend/web/js/turnto-config.js
+++ view/frontend/web/js/turnto-config.js
@@ -21,36 +21,48 @@ define([
         if (window.turnToConfig.hasOwnProperty('sso')
         ) {
             window.turnToConfig.sso.userDataFn = function (contextObj) {
-                $.get(window.turnToConfig.baseUrl + 'turnto/sso/getuserstatus', function (data) {
-                    if (data.jwt === null) {
-                        //user is logged out - redirect to login with message
-                        let context = JSON.parse(atob(contextObj));
-                        window.sessionStorage.setItem('contextObj', contextObj)
-                        window.location.replace("/turnto/sso/redirecttologin/action/" + context.action + '/authSetting/' + context.authSetting);
-                    } else if (window.sessionStorage.getItem('contextObj')) {
-                        //if user is coming from a log in redirect - use old context obj
-                        window.TurnToCmd('ssoRegDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: data.jwt});
-                        window.sessionStorage.removeItem('contextObj')
-                    } else {
-                        window.TurnToCmd('ssoRegDone', {context: contextObj, userDataToken: data.jwt});
-                    }
-                })
+                $.get(window.turnToConfig.baseUrl + 'turnto/sso/getuserstatus')
+                    .done(function (data) {
+                        if (data.jwt === null) {
+                            //user is logged out - redirect to login with message
+                            let context = JSON.parse(atob(contextObj));
+                            window.sessionStorage.setItem('contextObj', contextObj)
+                            window.location.replace("/turnto/sso/redirecttologin/action/" + context.action + '/authSetting/' + context.authSetting);
+                        } else if (window.sessionStorage.getItem('contextObj')) {
+                           //if user is coming from a log in redirect and the review window is triggered manually - use old context obj
+                           window.TurnToCmd('ssoRegDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: data.jwt});
+                            window.sessionStorage.removeItem('contextObj')
+                        } else {
+                         window.TurnToCmd('ssoRegDone', {context: contextObj, userDataToken: data.jwt});
+                        }
+                    })
+                    .fail(function () {
+                         window.TurnToCmd('ssoRegDone', {context: contextObj, userDataToken: null});
+                    });
 
             };
 
             //When a user is redirected to PDP after login
             if(window.sessionStorage.getItem('contextObj')){
-                $.get(window.turnToConfig.baseUrl + 'turnto/sso/getuserstatus', function (data) {
-                    window.TurnToCmd('ssoRegDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: data.jwt});
-                    window.sessionStorage.removeItem('contextObj');
-                })
+                $.get(window.turnToConfig.baseUrl + 'turnto/sso/getuserstatus')
+                    .done(function (data){
+                        window.TurnToCmd('ssoRegDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: data.jwt});
+                        window.sessionStorage.removeItem('contextObj');
+                    })
+                    .fail(function(){
+                        window.TurnToCmd('ssoRegDone', {context: window.sessionStorage.getItem('contextObj'), userDataToken: null});
+                        window.sessionStorage.removeItem('contextObj');
+                    });
             }
 
             window.turnToConfig.sso.loggedInDataFn = function(contextObj) {
-                $.get(window.turnToConfig.baseUrl + 'turnto/sso/loggedindata', function (data) {
+                $.get(window.turnToConfig.baseUrl + 'turnto/sso/loggedindata')
+                    .done(function (data) {
                         window.TurnToCmd('loggedInDataFnDone', {context: contextObj, userDataToken: data.jwt});
-                    }
-                )
+                    })
+                    .fail(function () {
+                        window.TurnToCmd('loggedInDataFnDone', {context: contextObj, userDataToken: null});
+                    })
             };
 
             window.turnToConfig.sso.logout =  function() {
