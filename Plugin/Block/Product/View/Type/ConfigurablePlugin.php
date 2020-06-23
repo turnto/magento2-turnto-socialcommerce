e9ccb7a385eb373eb91cf21f29832bac8f80651a
Josh Fedo <joshfedoCL@bitbucket.org>
2020-06-23 12:53:20 -0500

    TTOR01-29 - code clean up

--- Plugin/Block/Product/View/Type/ConfigurablePlugin.php
+++ Plugin/Block/Product/View/Type/ConfigurablePlugin.php
@@ -1,36 +1,53 @@
 <?php
 /**
  * @category    ClassyLlama
- * @package     
+ * @package
  * @copyright   Copyright (c) 2020 Classy Llama Studios, LLC
  */
 namespace TurnTo\SocialCommerce\Plugin\Block\Product\View\Type;
+
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\ConfigurableProduct\Block\Product\View\Type\Configurable;
+use TurnTo\SocialCommerce\Helper\Config;
 
 class ConfigurablePlugin
 {
 
     /**
-     * @var \TurnTo\SocialCommerce\Helper\Config
+     * @var Config
      */
     protected $config;
 
     /**
-     * @var \Magento\Catalog\Model\Product
+     * @var Product
      */
     protected $product;
 
-    public function __construct(\TurnTo\SocialCommerce\Helper\Config $config, \Magento\Catalog\Model\ProductRepository $product)
+    /**
+     * ConfigurablePlugin constructor.
+     *
+     * @param Config            $config
+     * @param ProductRepository $product
+     */
+    public function __construct(Config $config, ProductRepository $product)
     {
         $this->config = $config;
         $this->product = $product;
     }
 
+    /**
+     * @param Configurable $subject
+     * @param              $result
+     *
+     * @return false|string
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function afterGetJsonConfig(
-        \Magento\ConfigurableProduct\Block\Product\View\Type\Configurable $subject,
+        Configurable $subject,
         $result
-    )
-    {
-        $result = json_decode($result,true);
+    ) {
+        $result = json_decode($result, true);
         $result['useChild'] = (bool)$this->config->getUseChildSku();
         $result['parentSku'] = $this->product->getById($result['productId'])->getSku();
         return json_encode($result);
