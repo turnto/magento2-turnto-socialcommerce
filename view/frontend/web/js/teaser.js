fd107ad4fb1f20ff8f0852addbf8208cfa0965c4
Josh Fedo <joshfedoCL@bitbucket.org>
2020-10-30 16:17:13 -0500

    TTOR01-50 - added new js and refactored teaser

--- view/frontend/web/js/teaser.js
+++ view/frontend/web/js/teaser.js
@@ -38,13 +38,8 @@ define([
                 this.loadTeaserCounts(this.teaserSku);
             }
 
-            // Map bridge the tabs widget to something we can manually call
-            jQuery.widget.bridge('mage_tabs', jQuery.mage.tabs);
-            this.tabsContainer = document.querySelector('.product.data.items');
-
             return this;
         },
-
         loadTeaserCounts: function loadTeaserCounts(sku) {
             var xhr = new XMLHttpRequest();
 
@@ -54,56 +49,21 @@ define([
                     return;
                 }
                 this.reviewsData(JSON.parse(xhr.responseText));
-                this.populateReviewTabCount();
             }.bind(this));
             xhr.send();
         },
-
         getNumFullStars: function getFullStars() {
             //if the reviews are 4.8 or above return 5 else return the reviews
             return (this.reviewsData().avgRating >= 4.75) ? 5 : Math.round(this.reviewsData().avgRating);
         },
-
         hasHalfStar: function hasHalfStar() {
             let halfStarValue = (this.reviewsData().avgRating - this.getNumFullStars()).toFixed(2);
             return halfStarValue > 0.25 && halfStarValue <= .75;
         },
-
         getNumEmptyStars: function getNumEmptyStars() {
             return (5 - (this.getNumFullStars() + (this.hasHalfStar() ? 1 : 0)));
         },
 
-        writeReview: function writeReview() {
-            window.TurnToCmd('reviewsList.writeReview');
-        },
 
-        getTabIndex: function getTabIndex(tabAnchor) {
-            /** @type Array.<Element> */
-            var tabs = Array.prototype.slice.call(this.tabsContainer.querySelectorAll('.title a'));
-
-            // Start at the end going backwards so that if none are found we default to opening the first tab
-            var tabIndex = tabs.length - 1;
-
-            // Initializer intentionally left out for block scoping. Doesn't apply to var, but better practice to block
-            // scope always, even if there is no practical effect
-            for (; tabIndex >= 0; tabIndex--) {
-                if (tabs[tabIndex].getAttribute('href') === tabAnchor) {
-                    break;
-                }
-            }
-
-            return tabIndex;
-        },
-
-        openTab: function openTab(tabAnchor) {
-            jQuery(this.tabsContainer).mage_tabs('activate', this.getTabIndex(tabAnchor));
-            this.tabsContainer.scrollIntoView();
-        },
-
-        populateReviewTabCount: function populateReviewTabCount(){
-            let reviewTab = document.getElementById('tab-label-reviews-title');
-            let reviewCount = this.reviewsData().reviews;
-            reviewTab.innerHTML = reviewTab.innerHTML + '<span class="counter">'+ reviewCount +'</span>';
-        }
     });
 });
