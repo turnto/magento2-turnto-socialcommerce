2fb47d0ad9159056ed1ad6932866eba5d8c2a729
rholgate_turnto <rholgate@turnto.com>
2020-12-10 11:20:02 -0800

    Change local teaser vs widget teaser setting to be clearer

---
+++ Model/Config/Source/TeaserType.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * TurnTo_SocialCommerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright  Copyright (c) 2018 TurnTo Networks, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+namespace TurnTo\SocialCommerce\Model\Config\Source;
+
+/**
+ * Class ProductAttributeSelect
+ * @package TurnTo\SocialCommerce\Model\Config\Source
+ */
+class TeaserType implements \Magento\Framework\Option\ArrayInterface
+{
+
+    /**
+     * Options getter
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $optionArray = [
+            [
+                'value' => '0',
+                'label' => __('Use Local Teaser Code')
+            ],
+            [
+                'value' => '1',
+                'label' => __('Use Teaser Widget')
+            ]
+        ];
+
+        return $optionArray;
+    }
+}
